e4d99e2f250ebe885d251ea38c4df099249f8a24
vngarg <sngargrsd@gmail.com>
2020-10-30 00:13:53 +0530

    Updates

---
+++ precache-manifest.709340ab3c77fae3f07805cdd952012c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecb9249f14452c62c2d7dc8d89555e7",
+    "url": "/Hasura-Graphql-Todo-App/index.html"
+  },
+  {
+    "revision": "608de1574f0f74cb0bb1",
+    "url": "/Hasura-Graphql-Todo-App/static/css/2.371065e2.chunk.css"
+  },
+  {
+    "revision": "7bb3e85b8c4e2a265153",
+    "url": "/Hasura-Graphql-Todo-App/static/css/main.755d23fe.chunk.css"
+  },
+  {
+    "revision": "608de1574f0f74cb0bb1",
+    "url": "/Hasura-Graphql-Todo-App/static/js/2.fa8aa409.chunk.js"
+  },
+  {
+    "revision": "7a526f4dfa79ba43ded01cebbfe8fb7a",
+    "url": "/Hasura-Graphql-Todo-App/static/js/2.fa8aa409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb3e85b8c4e2a265153",
+    "url": "/Hasura-Graphql-Todo-App/static/js/main.4d7046f2.chunk.js"
+  },
+  {
+    "revision": "eaf0c0e549ae7129dc91",
+    "url": "/Hasura-Graphql-Todo-App/static/js/runtime-main.785bf269.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Hasura-Graphql-Todo-App/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Hasura-Graphql-Todo-App/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Hasura-Graphql-Todo-App/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Hasura-Graphql-Todo-App/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Hasura-Graphql-Todo-App/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Hasura-Graphql-Todo-App/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Hasura-Graphql-Todo-App/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Hasura-Graphql-Todo-App/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Hasura-Graphql-Todo-App/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Hasura-Graphql-Todo-App/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Hasura-Graphql-Todo-App/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Hasura-Graphql-Todo-App/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Hasura-Graphql-Todo-App/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Hasura-Graphql-Todo-App/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Hasura-Graphql-Todo-App/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Hasura-Graphql-Todo-App/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
